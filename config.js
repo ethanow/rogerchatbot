d22daced3fe0c8ff63a63b650fb45eddbb524f38
ethanow <owch0002@e.ntu.edu.sg>
2016-09-27 22:43:32 +0800

    configured tokens

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN || '32ELXJPENKMP6RGVETJV6QN5ZUIHZ6EE'
+const WIT_TOKEN = process.env.WIT_TOKEN ||
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAPCnjgJAvkBACZBidHZBZACz35igIGqULElTntgiAgVzIxVxAX7Y38Eb1N2FD2bKGZBT8OlL6a6F0qEJsWhYdWeZCdsbaZCTd13HT8QEwNPRa3owTNuAeqLG5HZAAzkOC1jiGhR90Nlb8mk3cylI0FsuZBf6lWWzMB5ZA3cXXVzgCQZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
